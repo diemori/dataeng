66ead6daa307bc0193890b3fc7e49ef38263b0f7
inwooro <inwooro@gmail.com>
2018-01-21 18:56:02 +0900

    setup target items

--- crawlers/linguist/culturefr/culturefr/items.py
+++ crawlers/linguist/culturefr/culturefr/items.py
@@ -11,4 +11,13 @@ import scrapy
 class CulturefrItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
+    word_name = scrapy.Field()
+    synonyme = scrapy.Field()
+    domain = scrapy.Field()
+    definition = scrapy.Field()
+    note = scrapy.Field()
+    voir_aussi = scrapy.Field()
+    equivalent = scrapy.Field()
+    journal_official_du = scrapy.Field()
+    source = scrapy.Field()
     pass
